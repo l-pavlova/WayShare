7f25444bbe0070bde11f51052507819a47b6f978
Lyudmila <lusito.pav@gmail.com>
2021-05-27 20:39:06 +0300

    Fix navigation items to show proper demo for when user is/isnt logged in

---
+++ hideProfile.js
@@ -0,0 +1,4 @@
+document.getElementById('show-profile').addEventListener('click', (event) => {
+    document.getElementById('login').style.display = none;
+    document.getElementById('sign').style.display = none;
+})
